439870d17dfc875cdeb9695c1a8a388e45a37bbf
Tiantian Liu <ltt1598@gmail.com>
2021-10-25 19:43:44 +0800

    Update 04_pretty_hip.py

--- 04_pretty_hip.py
+++ 04_pretty_hip.py
@@ -6,7 +6,7 @@ import handy_shader_functions as hsf
 ti.init(arch = ti.cuda)
 
 res_x = 512
-res_y = 512
+res_y = 256
 pixels = ti.Vector.field(3, ti.f32, shape=(res_x, res_y))
 
 @ti.kernel
@@ -16,6 +16,7 @@ def render(t:ti.f32):
         color = ti.Vector([0.0, 0.0, 0.0]) # init your canvas to black
         aspect = float(res_y) / float(res_x)
         uv = ti.Vector([float(i) / res_x, float(j) / res_x])
+
         shift = ti.Vector([0.5, 0.5*aspect])
         uv -= shift
         angle = 45.0 / 180.0 * 3.1415926 # 45 degrees in radius
@@ -23,6 +24,7 @@ def render(t:ti.f32):
         uv = R@uv
         uv += shift
         uv[1] += 0.5*(1-aspect)
+        
         pos = 10*uv
         rep = hsf.fract(pos)
         dist = 2.0*ti.min(ti.min(rep.x, 1.0-rep.x), ti.min(rep.y, 1.0-rep.y))
@@ -31,7 +33,6 @@ def render(t:ti.f32):
         edge = ti.sin(t - squareDist*0.5) + 0.5
         edge = 2.0*hsf.fract(edge*0.5)
         value = hsf.fract(dist*2.0)
-
 
         pixels[i,j] = color
         value = hsf.lerp(value, 1-value, hsf.step(1.0, edge))
