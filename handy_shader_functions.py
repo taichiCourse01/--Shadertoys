c7d2356b4ebea52bb8e21d021682ae669d1915da
Tiantian Liu <ltt1598@gmail.com>
2021-10-25 18:11:46 +0800

    Update handy_shader_functions.py

--- handy_shader_functions.py
+++ handy_shader_functions.py
@@ -1,19 +1,9 @@
 import taichi as ti
 
 @ti.func
-def swap(v1, v2):
-    return (v2, v1)
-
-@ti.func
-def clamp(v, v_min, v_max):
-    if v < v_min: v = v_min
-    if v > v_max: v = v_max
-    return v
-
-@ti.func
-def smoothstep(v1, v2, v):
-    assert(v1 != v2)
-    t = (v-v1) / float(v2-v1)
+def smoothstep(edge1, edge2, v):
+    assert(edge1 != edge2)
+    t = (v-edge1) / float(edge2-edge1)
     t = clamp(t, 0.0, 1.0)
 
     return (3-2 * t) * t**2
@@ -30,11 +20,15 @@ def lerp(x, y, a):
     return x * (1-a) + y * a
 
 @ti.func
-def clamp3(vec, v_min, v_max):
-    for i in ti.static(range(3)):
-        if vec[i] < v_min: vec[i] = v_min
-        if vec[i] > v_max: vec[i] = v_max
-    return vec
+def clamp(v, v_min, v_max):
+    if ti.static(isinstance(v, ti.Matrix)): # vector is implemented as a special type of ti.Matrix
+        for i in ti.static(range(v.m)): # v.m = number of rows for a vector
+            if v[i] < v_min: v[i] = v_min
+            if v[i] > v_max: v[i] = v_max
+    else: # scalar
+        if v < v_min: v = v_min
+        if v > v_max: v = v_max
+    return v
 
 @ti.func
 def floor(vec):
