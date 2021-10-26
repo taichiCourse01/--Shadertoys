ec62447d75010919405077b8294a6a6c532ccf67
Tiantian Liu <ltt1598@gmail.com>
2021-10-26 11:11:05 +0800

    Update quiz_fractal_tiling.py
    remove the tiling part, leave it as a quiz

--- quiz/quiz_fractal_tiling.py
+++ quiz/quiz_fractal_tiling.py
@@ -24,22 +24,8 @@ def render(t: ti.f32):
         row = i + offset
         col = j + offset
 
-        # for k in range(6):
-
-        #     pos = ti.Vector([hsf.mod(row, tile_size), hsf.mod(col, tile_size)]) # keeps the pos in [0, tile_size - 1]
-        #     tile_id = ti.Vector([row // tile_size, col//tile_size]) # save the tile ids as the random number seeds
-        #     uv = pos / float(tile_size) # uv coordinates in [0.0, 1.0])
-
-        #     time_dependent_rand = ti.sin(tile_id[0]*0.7 + tile_id[1]*3.1 + 0.01 * t)
-        #     square_color = hsf.fract(ti.Vector([0.1, 0.2, 0.3]) + time_dependent_rand) # add some randomness to the color
-        #     square_opacity = hsf.smoothstep(0.45, 0.55, hsf.fract(time_dependent_rand)) # add some randomness to the opacity
-
-        #     square_intensity = ti.sqrt(16.0 * uv[0]*uv[1]*(1.0-uv[0])*(1.0-uv[1])) # color intensity in [0.0, 1.0], brighter in the middle and dimmer in the corners
-
-        #     color += 2 * square_color * square_intensity * square_opacity # multiply by two because we are going to reduce it by half at the end
-
-        #     tile_size *= 2.0
-        #     color /= 2.0
+        for k in range(6):
+            ... # do something
 
         color = hsf.clamp(color, 0.0, 1.0)
 
