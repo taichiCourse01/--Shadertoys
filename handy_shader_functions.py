6b2df13ea87162410cb6cef7969330865ac62aae
Tiantian Liu <ltt1598@gmail.com>
2021-10-27 10:16:48 +0800

    correct smoothstep

--- handy_shader_functions.py
+++ handy_shader_functions.py
@@ -9,7 +9,7 @@ def smoothstep(edge1, edge2, v):
     return (3-2 * t) * t**2
 
 @ti.func
-def smoothstep(edge1, edge2, v):
+def linearstep(edge1, edge2, v):
     assert(edge1 != edge2)
     t = (v-edge1) / float(edge2-edge1)
     t = clamp(t, 0.0, 1.0)
